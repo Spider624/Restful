884d3e29d18ad0bd2f6ec17de840a4ed7136e347
Мамедов Дмитрий <mamedov@vbankcenter.ru>
2022-11-18 22:59:12 +0300

    Added. ex01. Authentication and authorization

--- src/main/java/edu/school21/restfull/config/SwaggerConfig.java
+++ src/main/java/edu/school21/restfull/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package edu.school21.restfull.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
